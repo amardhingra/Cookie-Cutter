a4c6ea7df3a2375adb262cad626868934089c988
Amar Dhingra <amardhingra@gmail.com>
2015-10-27 14:36:32 -0400

    Shapes

--- g5/Player.java
+++ g5/Player.java
@@ -9,52 +9,63 @@ import java.util.*;
 
 public class Player implements cc2.sim.Player {
 
-	private boolean[] row_2 = new boolean [0];
+    public Shape cutter(int length, Shape[] shapes, Shape[] opponentShapes) {
+        Shape shape = null;
+        if (length == 11) {
+            shape = ShapeGenerator.getNextElevenShape(shapes, opponentShapes);
+        } else if (length == 8) {
+            shape = ShapeGenerator.getNextEightShape(shapes, opponentShapes);
+        } else if (length == 5) {
+            shape = ShapeGenerator.getNextFiveShape(shapes, opponentShapes);
+        }
 
-	private Random gen = new Random();
+        System.out.println("Shape:" + shape);
+        return shape;
+    }
 
-	public Shape cutter(int length, Shape[] shapes, Shape[] opponentShapes)
-	{
-		Shape shape = null;
-		if(length == 11){
-			shape = ShapeGenerator.getNextElevenShape(shapes, opponentShapes);
-		} else if(length == 8){
-			shape = ShapeGenerator.getNextEightShape(shapes, opponentShapes);
-		} else if(length == 5){
-			shape = ShapeGenerator.getNextFiveShape(shapes, opponentShapes);
-		}
+    public Move getBestMove(Dough dough, ArrayList<Move> elevenMoves, ArrayList<Move> eightMoves, ArrayList<Move> fiveMoves){
+        if(elevenMoves.size() > 0) {
+            return elevenMoves.get(0);
+        } else if(eightMoves.size() > 0){
+            return eightMoves.get(0);
+        } else {
+            return fiveMoves.get(0);
+        }
+    }
 
-		return shape;
-	}
+    public Move cut(Dough dough, Shape[] shapes, Shape[] opponent_shapes) {
 
-	public Move cut(Dough dough, Shape[] shapes, Shape[] opponent_shapes)
-	{
-		// prune larger shapes if initial move
-		if (dough.uncut()) {
-			int min = Integer.MAX_VALUE;
-			for (Shape s : shapes)
-				if (min > s.size())
-					min = s.size();
-			for (int s = 0 ; s != shapes.length ; ++s)
-				if (shapes[s].size() != min)
-					shapes[s] = null;
-		}
-		// find all valid cuts
-		ArrayList <Move> moves = new ArrayList <Move> ();
-		for (int i = 0 ; i != dough.side() ; ++i)
-			for (int j = 0 ; j != dough.side() ; ++j) {
-				Point p = new Point(i, j);
-				for (int si = 0 ; si != shapes.length ; ++si) {
-					if (shapes[si] == null) continue;
-					Shape[] rotations = shapes[si].rotations();
-					for (int ri = 0 ; ri != rotations.length ; ++ri) {
-						Shape s = rotations[ri];
-						if (dough.cuts(s, p))
-							moves.add(new Move(si, ri, p));
-					}
-				}
-			}
-		// return a cut randomly
-		return moves.get(gen.nextInt(moves.size()));
-	}
+        ArrayList<Move> elevenMoves = new ArrayList<>();
+        ArrayList<Move> eightMoves = new ArrayList<>();
+        ArrayList<Move> fiveMoves = new ArrayList<>();
+
+        for (int i = 0; i < dough.side(); i++) {
+            for (int j = 0; j < dough.side(); j++) {
+                Point p = new Point(i, j);
+
+                for (int shapeNumber = 0; shapeNumber < shapes.length; shapeNumber++) {
+                    Shape[] rotations = shapes[shapeNumber].rotations();
+                    for (int rotNumber = 0; rotNumber < rotations.length; rotNumber++) {
+                        if (dough.cuts(rotations[rotNumber], p)) {
+                            switch (shapes[shapeNumber].size()) {
+                                case 11:
+                                    elevenMoves.add(new Move(shapeNumber, rotNumber, p));
+                                    break;
+                                case 8:
+                                    eightMoves.add(new Move(shapeNumber, rotNumber, p));
+                                    break;
+                                case 5:
+                                default:
+                                    fiveMoves.add(new Move(shapeNumber, rotNumber, p));
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return getBestMove(dough, elevenMoves, eightMoves, fiveMoves);
+
+    }
 }
